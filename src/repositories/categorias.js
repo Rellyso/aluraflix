d9057fb6921415785bbc6cf5076248cc5b0e31f2
Rellyson <rellysondouglas2015@gmail.com>
2020-07-31 17:54:34 -0300

    Added repositories' folder

---
+++ src/repositories/categorias.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import config from '../config';
+
+function getAllWithVideos() {
+  console.log(config.URL_BACKEND_TOP);
+
+  return config.URL_BACKEND_TOP;
+}
+
+export default {
+  getAllWithVideos,
+};
