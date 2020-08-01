4c5058aa1694531f26ae98532990b267f46ef71e
Rellyson <rellysondouglas2015@gmail.com>
2020-08-01 11:10:32 -0300

    Updating

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './reset.css';
 
 import Home from './pages/Home';
-import CadastroVideo from './pages/cadastro/Video'
+import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-const Pagina404 = () => (<div>Página 404</div>)
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/cadastro/video" component={CadastroVideo}/>
-      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
-      <Route component={Pagina404}/>
+      <Route path="/" exact component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
